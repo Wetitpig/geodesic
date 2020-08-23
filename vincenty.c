931dfb5fcfaceb3eda85a4b46aabe97962740e71
Wetitpig <winsto003@hotmail.com>
2020-08-23 23:37:10 +0800

    Add antinodal solution for vincenty formula

--- vincenty.c
+++ vincenty.c
@@ -13,9 +13,11 @@ int main(int argc, char **argv)
 	}
 
 	struct Coordinates *location = malloc(sizeof(struct Coordinates) * 2);
-	long double londiff, lambda, oldlambda, u1, u2;
+	long double londiff, lambda, oldvalue, u1, u2;
 	long double ssig, csig, sig, salp, calp, cos2, C;
 	long double usq, k1, a, b, dsig, s;
+
+	long double d = 0;
 	long double total = 0;
 
 	scan(argc, argv[1], location);
@@ -38,14 +40,9 @@ int main(int argc, char **argv)
 		u2 = atanl((1 - FLAT) * tanl((location + 1)->lat));
 
 		do {
-			oldlambda = lambda;
+			oldvalue = lambda;
 
-			ssig = cosl(u2) * sinl(lambda);
-			csig = cosl(u1) * sinl(u2) - sinl(u1) * cosl(u2) * cosl(lambda);
-			ssig *= ssig;
-			csig *= csig;
-			ssig = sqrtl(ssig + csig);
-
+			ssig = sqrtl(sqr(cosl(u2) * sinl(lambda)) + sqr(cosl(u1) * sinl(u2) - sinl(u1) * cosl(u2) * cosl(lambda)));
 			csig = sinl(u1) * sinl(u2) + cosl(u1) * cosl(u2) * cosl(lambda);
 
 			sig = atan2l(ssig, csig);
@@ -56,10 +53,46 @@ int main(int argc, char **argv)
 
 			C = FLAT / 16 * calp * (4 + FLAT * (4 - 3 * calp));
 			lambda = londiff + (1 - C) * FLAT * salp * (sig + C * ssig * (cos2 + C * csig * (2 * cos2 * cos2 - 1)));
-		} while (fabsl(oldlambda - lambda) >= powl(10,-12));
+
+			if (fabsl(lambda) > M_PI) {
+				d = 1;
+				break;
+			}
+		} while (fabsl(oldvalue - lambda) >= powl(10,-12));
+
+		if (d == 1) {
+			londiff = (londiff > 0 ? M_PI : -M_PI) - londiff;
+			lambda = 0;
+
+			calp = 0.5;
+			cos2 = 0;
+
+			sig = M_PI - fabsl(u1 + u2);
+			ssig = sinl(sig);
+			csig = cosl(sig);
+
+			do {
+				oldvalue = salp;
+
+				C = FLAT / 16 * calp * (4 + FLAT * (4 - 3 * calp));
+				cos2 = csig - 2 * sinl(u1) * sinl(u2) / calp;
+
+				d = (1 - C) * FLAT * (sig + C * ssig * (cos2 + C * csig * (2 * cos2 * cos2 - 1)));
+				salp = (londiff - asinl(lambda)) / d;
+				calp = 1 - salp * salp;
+
+				lambda = salp * ssig / (cosl(u1) * cosl(u2));
+
+				ssig = sqr(cosl(u2) * lambda) + sqr(cosl(u1) * sinl(u2) + sinl(u1) * cosl(u2) * sqrtl(1 - sqr(lambda)));
+				csig = -sqrtl(1 - ssig);
+				ssig = sqrtl(ssig);
+				sig = M_PI - asinl(ssig);
+
+			} while (fabsl(oldvalue - salp) >= powl(10,-12));
+		}
 
 		usq = calp * (RAD_MAJ * RAD_MAJ / (RAD_MIN * RAD_MIN) - 1);
-		k1 = sqrt(1 + usq);
+		k1 = sqrtl(1 + usq);
 		k1 = (k1 - 1) / (k1 + 1);
 		a = (1 + k1 * k1 / 4) / (1 - k1);
 		b = k1 * (1 - 3 / 8 * k1 * k1);
