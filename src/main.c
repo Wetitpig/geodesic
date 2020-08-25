fa6329248a1f4597204486fb551ed302c85c49e3
Wetitpig <winsto003@hotmail.com>
2020-08-25 12:13:16 +0800

    Beautify main.c

--- src/main.c
+++ src/main.c
@@ -68,148 +68,147 @@ int main(int argc, char **argv)
 
 	switch (p)
 	{
-	case 1:
-	if (distance == 0 && azimuth == 0) {
-		puts("Nothing to be shown. Abort.");
-		exit(1);
-	}
-
-	struct Coordinates *location = malloc(sizeof(struct Coordinates) * 2);
-
-	if (scan_coordinates(optind == argc, argv[optind], location) != 2) {
-		puts("Incorrect format of coordinates");
-		exit(1);
-	}
-
-	putchar('{');
-
-	for (i = 1; optind == argc || (optind + i) < argc; ++i) {
-		if (optind != argc) {
-			if (scan_coordinates(argc, argv[optind + i], location + 1) != 2) {
-				putchar('\b');
-				puts("Incorrect format of coordinates");
-				exit(1);
-			}
+		case 1:
+		if (distance == 0 && azimuth == 0) {
+			puts("Nothing to be shown. Abort.");
+			exit(1);
 		}
-		else if (scan_coordinates(1, NULL, location + 1) == -1)
-			break;
 
-		if (azimuth == 1 && i != 1)
-			printf(",");
+		struct Coordinates *location = malloc(sizeof(struct Coordinates) * 2);
 
-		printf("\n  \"%d\": {\n", i - 1);
-
-		switch (j)
-		{
-			case 0:
-			if (distance == 1)
-				c = haversine_inverse_distance(location);
-			if (azimuth == 1) {
-				start = NORMALISE_A(haversine_inverse_bearing(location, location + 1));
-				end = NORMALISE_A(haversine_inverse_bearing(location + 1, location) - 180);
-			}
-			break;
-			case 1:
-			res = vincenty_inverse(location);
-			c = res.distance;
-			start = NORMALISE_A(res.start);
-			end = NORMALISE_A(res.end);
-			break;
+		if (scan_coordinates(optind == argc, argv[optind], location) != 2) {
+			puts("Incorrect format of coordinates");
+			exit(1);
 		}
+
+		putchar('{');
+
+		for (i = 1; optind == argc || (optind + i) < argc; ++i) {
+			if (optind != argc) {
+				if (scan_coordinates(argc, argv[optind + i], location + 1) != 2) {
+					putchar('\b');
+					puts("Incorrect format of coordinates");
+					exit(1);
+				}
+			}
+			else if (scan_coordinates(1, NULL, location + 1) == -1)
+				break;
+
+			if (azimuth == 1 && i != 1)
+				printf(",");
+
+			printf("\n  \"%d\": {\n", i - 1);
+
+			switch (j)
+			{
+				case 0:
+				if (distance == 1)
+					c = haversine_inverse_distance(location);
+				if (azimuth == 1) {
+					start = NORMALISE_A(haversine_inverse_bearing(location, location + 1));
+					end = NORMALISE_A(haversine_inverse_bearing(location + 1, location) - 180);
+				}
+				break;
+				case 1:
+				res = vincenty_inverse(location);
+				c = res.distance;
+				start = NORMALISE_A(res.start);
+				end = NORMALISE_A(res.end);
+				break;
+			}
+
+			if (distance == 1) {
+				total += c;
+				printf("    \"distance\": %Lf", c);
+				if (azimuth == 1)
+					printf(",\n");
+				else
+					printf("\n  },");
+			}
+
+			if (azimuth == 1)
+				printf("    \"start_azimuth\": %Lf,\n    \"end_azimuth\": %Lf\n  }", start, end);
+
+			memcpy(location, location + 1, sizeof(struct Coordinates));
+		}
+
+		free(location);
 
 		if (distance == 1) {
-			total += c;
-			printf("    \"distance\": %Lf", c);
-			if (azimuth == 1)
-				printf(",\n");
-			else
-				printf("\n  },");
+			if (azimuth == 1 && total != 0)
+				printf(",");
+			printf("\n  \"total_distance\": %Lf\n}\n", total);
+		}
+		else
+			printf("\n}\n");
+		break;
+
+		case 0:
+		if (distance == 0 && azimuth == 0) {
+			puts("Nothing to be shown. Abort.");
+			exit(1);
 		}
 
-		if (azimuth == 1)
-			printf("    \"start_azimuth\": %Lf,\n    \"end_azimuth\": %Lf\n  }", start, end);
+		long double end;
 
-		memcpy(location, location + 1, sizeof(struct Coordinates));
-	}
+		struct Coordinates *point = malloc(sizeof(struct Coordinates) * 2);
+		struct Vector *add = malloc(sizeof(struct Vector));
 
-	free(location);
+		if (scan_coordinates(optind == argc, argv[optind], point) != 2) {
+			puts("Incorrect format of coordinates");
+			exit(1);
+		}
 
-	if (distance == 1) {
-		if (azimuth == 1 && total != 0)
-			printf(",");
-		printf("\n  \"total_distance\": %Lf\n}\n", total);
-	}
-	else
+		putchar('{');
+
+		for (i = 1; optind == argc || (optind + i) < argc; i++) {
+			if (optind != argc) {
+				if (scan_vector(argc, argv[optind + i], add) != 2) {
+					putchar('\b');
+					puts("Incorrect format of vector.");
+					exit(1);
+				}
+			}
+			else if (scan_vector(1, NULL, add) == -1)
+				break;
+
+			if (i != 1)
+				printf(",\n");
+
+			printf("\n  \"%d\": {\n", i - 1);
+
+			switch (j)
+			{
+				case 0:
+				*(point + 1) = haversine_direct(point, add);
+				if (azimuth == 1)
+					end = NORMALISE_A(haversine_inverse_bearing(point + 1, point) - 180);
+				break;
+				case 1:
+				res = vincenty_direct(point, add);
+				(point + 1)->lat = res.distance;
+				(point + 1)->lon = res.start;
+				end = NORMALISE_A(res.end);
+				break;
+			}
+			if (distance == 1) {
+				printf("    \"coordinate\": [%Lf,%Lf]", (point + 1)->lat / (RAD), (point + 1)->lon / (RAD));
+				if (azimuth == 1)
+					printf(",\n");
+			}
+
+			if (azimuth == 1)
+				printf("    \"azimuth\": %Lf", end);
+
+			printf("\n  }");
+			memcpy(point, point + 1, sizeof(struct Coordinates));
+		}
+
+		free(point);
+		free(add);
+
 		printf("\n}\n");
-	break;
-
-	case 0:
-	if (distance == 0 && azimuth == 0) {
-		puts("Nothing to be shown. Abort.");
-		exit(1);
+		break;
 	}
-
-	long double end;
-
-	struct Coordinates *point = malloc(sizeof(struct Coordinates) * 2);
-	struct Vector *add = malloc(sizeof(struct Vector));
-
-	if (scan_coordinates(optind == argc, argv[optind], point) != 2) {
-		puts("Incorrect format of coordinates");
-		exit(1);
-	}
-
-	putchar('{');
-
-	for (i = 1; optind == argc || (optind + i) < argc; i++) {
-		if (optind != argc) {
-			if (scan_vector(argc, argv[optind + i], add) != 2) {
-				putchar('\b');
-				puts("Incorrect format of vector.");
-				exit(1);
-			}
-		}
-		else if (scan_vector(1, NULL, add) == -1)
-			break;
-
-		if (i != 1)
-			printf(",\n");
-
-		printf("\n  \"%d\": {\n", i - 1);
-
-		switch (j)
-		{
-			case 0:
-			*(point + 1) = haversine_direct(point, add);
-			if (azimuth == 1)
-				end = NORMALISE_A(haversine_inverse_bearing(point + 1, point) - 180);
-			break;
-			case 1:
-			res = vincenty_direct(point, add);
-			(point + 1)->lat = res.distance;
-			(point + 1)->lon = res.start;
-			end = NORMALISE_A(res.end);
-			break;
-		}
-		if (distance == 1) {
-			printf("    \"coordinate\": [%Lf,%Lf]", (point + 1)->lat / (RAD), (point + 1)->lon / (RAD));
-			if (azimuth == 1)
-				printf(",\n");
-		}
-
-		if (azimuth == 1)
-			printf("    \"azimuth\": %Lf", end);
-
-		printf("\n  }");
-		memcpy(point, point + 1, sizeof(struct Coordinates));
-	}
-
-	free(point);
-	free(add);
-
-	printf("\n}\n");
-	break;
-	}
-
 	exit(0);
 }
