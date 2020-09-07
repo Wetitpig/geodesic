cb51d7932e365c576484a540a63d21f871de24e4
Wetitpig <winsto003@hotmail.com>
2020-09-07 17:30:54 +0800

    Remove constant-based expressions

--- src/vincenty.c
+++ src/vincenty.c
@@ -42,49 +42,39 @@ long double cos_reduced_latitude(long double lat)
 		return 1.0l / hypotl(1.0l, tan_reduced_latitude(lat));
 }
 
-long double helmertA(long double calp)
+long double helmertA(long double k1)
 {
 	long double A = 0;
-	long double usq, k1;
 	int k;
 
-	usq = calp * ECC2;
-	k1 = sqrtl(1.0l + usq);
-	k1 = (k1 - 1.0l) / (k1 + 1.0l);
-	for (k = 0; k < 8; k++)
+	for (k = 0; k < 5; k++)
 		A += sqr(double_fac(2 * k - 3) / double_fac(2 * k) * powl(k1, k));
-	A /= (1 - k1);
 	return A;
 }
 
-long double helmertB(long double calp, long double sig, long double A)
+long double helmertB(long double k1, long double sig)
 {
 	long double B = 0;
-	long double usq, k1, c;
+	long double c, c1 = 0;
 	int k, j;
 
-	usq = calp * ECC2;
-	k1 = sqrtl(1.0l + usq);
-	k1 = (k1 - 1.0l) / (k1 + 1.0l);
-	A *= (1 - k1);
-	for (k = 1; k < 17; k++) {
-		c = double_fac(2 * k - 3) / double_fac(2 * k) * powl(k1, k);
-		for (j = 1; (2 * j + k) < 17; j++)
-			c -= double_fac(2 * (j + k) - 3) / double_fac(2 * (j + k)) * double_fac(2 * j - 3) / double_fac(2 * j) * powl(k1, 2 * j + k);
-		B += c / (A * k) * sinl(2.0l * k * sig);
+	for (k = 1; k < 9; k++) {
+		c = double_fac(2 * k - 3) / double_fac(2 * k);
+		for (j = 1; (2 * j + k) < 9 && c1 != c; j++) {
+			c1 = c;
+			c -= double_fac(2 * (j + k) - 3) / double_fac(2 * (j + k)) * double_fac(2 * j - 3) / double_fac(2 * j) * powl(k1, 2 * j);
+		}
+		B += c / k * sinl(2.0l * k * sig) * powl(k1, k);
 	}
 	return B;
 }
 
-long double helmertB1(long double calp, long double sig)
+long double helmertB1(long double k1, long double sig)
 {
 	long double B = 0;
-	long double usq, k1, c;
+	long double c;
 	int k, j, i;
 
-	usq = calp * ECC2;
-	k1 = sqrtl(1.0l + usq);
-	k1 = (k1 - 1.0l) / (k1 + 1.0l);
 	for (k = 1; k < 9; k++) {
 		c = 0;
 		i = 0;
@@ -104,7 +94,7 @@ void vincenty_inverse(struct Coordinates *location, struct Coordinates *location
 {
 	long double londiff, lambda, u1, u2, cu1, cu2, su1, su2;
 	long double ssig, csig, sig, salp, calp, cos2, c;
-	long double a, b, dsig, s;
+	long double a, b, k1, s;
 	int i = 0;
 
 	long double oldvalue[2];
@@ -178,10 +168,11 @@ void vincenty_inverse(struct Coordinates *location, struct Coordinates *location
 		} while (fabsl(oldvalue[0] - salp) >= powl(2,-48));
 	}
 
-	a = helmertA(calp);
-	b = helmertB(calp, sig, a);
+	k1 = calp * ECC2;
+	k1 = sqrtl(1.0l + k1);
+	k1 = (k1 - 1.0l) / (k1 + 1.0l);
 
-	s = RAD_MIN * a * (sig - b);
+	s = RAD_MIN * (helmertA(k1) * sig - helmertB(k1, sig)) / (1.0l - k1);
 
 	if (i != 16384) {
 		a = cu2 * sinl(lambda);
@@ -212,9 +203,10 @@ void vincenty_inverse(struct Coordinates *location, struct Coordinates *location
 
 void vincenty_direct(struct Coordinates *point, struct Vector *add, long double *res)
 {
-	long double u1, su1, cu1, s1, s2, salp, a;
-	long double sigma, oldvalue;
+	long double u1, su1, cu1, s1, s2, salp, a, b, k1, k2, usq;
+	long double sigma, sig0, oldvalue;
 	long double lambda, c;
+	int i;
 
 	u1 = reduced_latitude(point->lat);
 	su1 = sin_reduced_latitude(point->lat);
@@ -222,9 +214,20 @@ void vincenty_direct(struct Coordinates *point, struct Vector *add, long double 
 	s1 = atan2_modified(tan_reduced_latitude(point->lat), cosl(add->theta));
 	salp = cu1 * sinl(add->theta);
 
-	a = helmertA(1.0l - sqr(salp));
-	sigma = add->s / (RAD_MIN * a);
-	sigma += helmertB1(1 - sqr(salp), sigma);
+	usq = (1.0l - sqr(salp)) * ECC2;
+	k1 = sqrtl(1.0l + usq);
+	k1 = (k1 - 1.0l) / (k1 + 1.0l);
+	k2 = 1.0l - k1;
+
+	a = helmertA(k1);
+	sig0 = add->s / RAD_MIN;
+	sigma = k2 * sig0 / a;
+	do {
+		oldvalue = sigma;
+		sigma += (sig0 - (a * sigma - helmertB(k1, sigma)) / k2) / sqrt(1.0l + usq * sqr(sinl(sigma)));
+	} while (fabsl(oldvalue - sigma) >= powl(2, -48));
+
+//	sigma += helmertB1(k1, sigma);
 	s2 = 2.0l * s1 + sigma;
 
 	*res = atan2_modified(su1 * cosl(sigma) + cu1 * sinl(sigma) * cosl(add->theta), (1 - FLAT) * hypotl(salp, su1 * sinl(sigma) - cu1 * cosl(sigma) * cosl(add->theta)));
